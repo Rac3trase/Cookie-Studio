6e1d69199f97c64cbd134e9b08e66b8070060daa
Rac3trase <161275083+Rac3trase@users.noreply.github.com>
2024-03-01 01:36:29 +0000

    a

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function button1Click() {
     alert("Hello")
 }
+
+function openNav() {
+    if (document.getElementById("mySidepanel").style.width != "250px")
+        document.getElementById("mySidepanel").style.width = "250px";
+    else {
+        closeNav()
+    }
+}
+
+function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+}
