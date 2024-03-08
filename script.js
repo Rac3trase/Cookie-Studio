b16b310c5424b58cb98e6f8197d7fdc46c61f483
Rac3trase <161275083+Rac3trase@users.noreply.github.com>
2024-03-08 02:41:54 +0000

    a

--- script.js
+++ script.js
@@ -40,40 +40,49 @@ function openUnblock() {
 function open1v1() {
     var url = "https://foxmoss.com/main/games/1v1lol/";
     var urlObj = new window.URL(window.location.href);
+
     if (url.substring(0, 8) !== "https://" && url.substring(0, 7) !== "http://") {
         url = "https://" + url.split("https://").pop();
     } else if (url.substring(0, 7) == "http://") {
         url = "https://" + url.split("http://").pop();
-    };
+    }
 
-    win = window.open();
+    var win = window.open();
     win.document.body.style.margin = "0";
     win.document.body.style.height = "100vh";
+
     var iframe = win.document.createElement("iframe");
     iframe.style.border = "none";
     iframe.style.width = "100%";
     iframe.style.height = "100%";
     iframe.style.margin = "0";
     iframe.referrerpolicy = "no-referrer";
-    iframe.allow = "fullscreen";
+    iframe.allowFullscreen = true;
     iframe.src = url;
+
     win.document.body.appendChild(iframe);
 
     var script = win.document.createElement("script");
-    
     script.innerHTML = `
-    function removeAds() {
-        var elements = document.getElementsByClassName('ads');
-
-        // Continue removing elements until there are none left
-        while (elements.length > 0) {
-            var parent = elements[0].parentNode;
-            parent.removeChild(elements[0]);
+        function removeAds() {
+            var elements = document.getElementsByClassName('ads');
+    
+            // Continue removing elements until there are none left
+            while (elements.length > 0) {
+                var parent = elements[0].parentNode;
+                parent.removeChild(elements[0]);
+            }
         }
-    }
-
-    // Call the function every half a second (500 milliseconds)
-    setInterval(removeAds, 500);
+    
+        // Use requestAnimationFrame for smoother execution
+        function mainLoop() {
+            removeAds();
+            // Add any other logic related to the iframe here
+            requestAnimationFrame(mainLoop);
+        }
+    
+        // Start the main loop
+        mainLoop();
     `;
 
     win.document.body.appendChild(script);
