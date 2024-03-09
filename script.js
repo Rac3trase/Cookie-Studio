be8a958168dee2810f18ac6f82a7aa593389a1a3
Rac3trase <161275083+Rac3trase@users.noreply.github.com>
2024-03-09 10:33:06 +0000

    a

--- script.js
+++ script.js
@@ -398,6 +398,38 @@ function openKirka() {
     win.document.body.appendChild(iframe);
 }
 
+function openUnblockGoogle() {
+    var url = "https://totallyscience.co/service/hvtrs8%2F-wuw%2Cgmoelg.aoo%2F/";
+    var urlObj = new window.URL(window.location.href);
+    if (url.substring(0, 8) !== "https://" && url.substring(0, 7) !== "http://") {
+        url = "https://" + url.split("https://").pop();
+    } else if (url.substring(0, 7) == "http://") {
+        url = "https://" + url.split("http://").pop();
+    };
+
+    win = window.open();
+    win.document.body.style.margin = "0";
+    win.document.body.style.height = "100vh";
+    win.document.title = "Calculator";
+    var iframe = win.document.createElement("iframe");
+    iframe.style.border = "none";
+    iframe.style.width = "100%";
+    iframe.style.height = "100%";
+    iframe.style.margin = "0";
+    iframe.referrerpolicy = "no-referrer";
+    iframe.allow = "fullscreen";
+    iframe.src = url;
+
+    var favi;
+    favi = win.document.createElement('link');
+    favi.rel = 'icon';
+    win.document.head.appendChild(favi);
+
+    favi.href = '/Images/Calculator.png';
+
+    win.document.body.appendChild(iframe);
+}
+
 function openNav() {
     if (document.getElementById("mySidepanel").style.width != "250px") {
         openNavAfter();
