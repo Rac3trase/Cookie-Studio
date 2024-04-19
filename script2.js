b99c1eb22c3339ceb7c71b75c78e6aeb58dfe28e
Rac3trase <161275083+Rac3trase@users.noreply.github.com>
2024-04-19 04:16:25 +0000

    a

---
+++ script2.js
@@ -0,0 +1,19 @@
+function open1v1lolNEW() {
+    const list = ["https://buildnow.netlify.app/", "https://rac3trase.github.io/1v1.lol/", "https://buildnow.glitch.me/"];
+    openUrl(getRList(list));
+}
+
+function openjustfallNEW() {
+    const list = ["https://server1-lol.glitch.me/", "https://rac3trase.github.io/justfall.lol/", "https://justfall-lol.netlify.app/"];
+    openUrl(getRList(list));
+}
+
+function openIntersteller() {
+    const list = ["https://interstellar-three-virid.vercel.app/"];
+    openUrl(getRList(list));
+}
+
+function openDoge() {
+    const list = ["https://doge-theta-five.vercel.app/"];
+    openUrl(getRList(list));
+}
